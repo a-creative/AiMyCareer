0ed6acd37b2ffe850e9a7acfa9be21b08ed843f3
Maya Kathrine Andersen <m.andersen.post@gmail.com>
2020-10-13 13:49:45 +0200

    - Update job posting in db

--- backend/src/app/Http/Controllers/JobPostingController.php
+++ backend/src/app/Http/Controllers/JobPostingController.php
@@ -7,20 +7,17 @@ use LaravelDoctrine\ORM\Facades\EntityManager;
 
 class JobPostingController extends Controller
 {
-
-    public function index() {
-
+    public function index()
+    {
         $result = EntityManager::createQueryBuilder()
             ->select('jp')
-            ->from( JobPosting::class, 'jp')
+            ->from(JobPosting::class, 'jp')
             ->getQuery()
-            ->getResult(\Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY);
+            ->getResult(\Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY)
+        ;
 
-        $result = [
-            'posting' => [ 'postings' => $result ]
+        return [
+            'posting' => ['postings' => $result],
         ];
-
-        return $result;
     }
-
 }
