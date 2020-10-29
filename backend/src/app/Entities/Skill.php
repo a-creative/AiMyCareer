74290a6c1c8ff52aa11f3178dab77fe26243670a
a-creative <m.andersen.post@gmail.com>
2020-10-30 00:17:38 +0100

    Skill type error handling added

--- backend/src/app/Entities/Skill.php
+++ backend/src/app/Entities/Skill.php
@@ -4,6 +4,12 @@ namespace App\Entities;
 
 use Illuminate\Contracts\Support\Arrayable;
 
+const TOOL_SKILL_TYPE = 0;
+const PERSONAL_INTERNAL_SKILL_TYPE = 1;
+const PERSONAL_EXTERNAL_SKILL_TYPE = 2;
+const LANGUAGE_SKILL_TYPE = 3;
+const OTHER_SKILL_TYPE = 4;
+const VALID_SKILL_TYPES = [ 0,1,2,3,4 ];
 class Skill implements Arrayable
 {
     protected $id;
@@ -23,5 +29,29 @@ class Skill implements Arrayable
 
         return $output;
 
+    }
+
+    /**
+     * Get the value of type
+     */ 
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @return  self
+     */ 
+    public function setType( $type )
+    {
+        if ( !in_array( $type, VALID_SKILL_TYPES ) ) {
+            throw new \TypeError( 'Skill type ' . $this->type . ' is not valid. Valid types are id: ' . implode(', ' , VALID_SKILL_TYPES ) );
+        }
+
+        $this->type = $type;
+
+        return $this;
     }
 }
